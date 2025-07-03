91bba788080fcba7b2ca63022503ef9423f99a0a
milandhangar123 <ms8755301254@gmail.com>
2025-07-03 20:20:02 +0530

    third

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcryptjs');
+
 
 const validator = require("validator");
 const userModel = require("../models/userModel");
